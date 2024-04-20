a225dc1d047cc23850173cdf6f8e4e7d46ad596b
Tiago Abreu <devtiagoabreu@gmail.com>
2024-04-20 07:17:29 -0300

    iniciando mineração de dados

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 # Créditos
 https://www.youtube.com/watch?v=SIUNTvtCM6c
+https://pt.stackoverflow.com/questions/495747/beautifulsoup-pegar-text-dentro-de-tabela
+https://www.youtube.com/watch?v=WzUPnQwSQyk
+https://www.youtube.com/watch?v=kqvWOcPog4s
--- requisicoes.py
+++ requisicoes.py
@@ -1,14 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-responseTear001ManCounter = requests.get('http://172.17.1.1/cgi-bin/main.cgi?294')
+#responseTear001ManCounter = requests.get('http://172.17.1.1/cgi-bin/main.cgi?294') http://94550ac37bb5.sn.mynetname.net:30002/cgi-bin/main.cgi?298
+responseTear001ManCounter = requests.get('http://94550ac37bb5.sn.mynetname.net:30002/cgi-bin/main.cgi?294')
 
 contentTear001ManCounter = responseTear001ManCounter.content
 
 Tear001ManCounter = BeautifulSoup(contentTear001ManCounter, 'html.parser')
 
-#print(Tear001ManCounter.prettify())
+#print(Tear001ManCounter.find_all("table"))
 
-table = (
-  contentTear001ManCounter.find("table")
-)
+print(Tear001ManCounter.find("table", attrs={}))
+
+
+
